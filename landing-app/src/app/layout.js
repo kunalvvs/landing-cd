27c1f25aab447384e0d9cf395ff44a733671c3a0
kunalvvs <kunalvvs@gmail.com>
2026-06-10 20:18:11 +0530

    j

--- landing-app/src/app/layout.js
+++ landing-app/src/app/layout.js
@@ -73,7 +73,7 @@ export default function RootLayout({ children }) {
         ${funnelSans.variable}`}>
       <body>
         <SmoothScroller />
-        <PageAnimations />
+        {/* <PageAnimations /> */}
         {children}
       </body>
     </html>
