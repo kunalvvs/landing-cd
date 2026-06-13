dad71cb463874e006179e7ebd2d1e839f8ec5189
kunalvvs <kunalvvs@gmail.com>
2026-06-13 10:43:31 +0530

    s

--- landing-app/src/app/page.js
+++ landing-app/src/app/page.js
@@ -3,7 +3,7 @@ import HeroSection from "@/components/sections/hero/HeroSection";
 import HowItWorksSection from "@/components/sections/how-it-works/HowItWorksSection";
 import FeaturesAutomationSection from "@/components/sections/features-automation/FeaturesAutomationSection";
 import FeaturesOverviewSection from "@/components/sections/features-overview/FeaturesOverviewSection";
-import SecurityWorkflowSection from "@/components/sections/security-workflow/SecurityWorkflowSection";
+// import SecurityWorkflowSection from "@/components/sections/security-workflow/SecurityWorkflowSection";
 import TestimonialsSection from "@/components/sections/testimonials/TestimonialsSection";
 import BlogSection from "@/components/sections/blog/BlogSection";
 import FAQSection from "@/components/sections/faq/FAQSection";
